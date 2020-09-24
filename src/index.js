accfb26f964305a6a22e354bb7679bd2b8147fd2
Surajv <vsuraj2405@gmail.com>
2020-09-24 18:32:48 +0530

    start

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<h1>Hello bro</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
