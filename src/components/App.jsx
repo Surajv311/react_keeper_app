bc256c648d0be459b28a90eee927ad51e67e2ad9
Surajv <vsuraj2405@gmail.com>
2020-11-12 15:32:48 +0530

    imported createArea component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,57 @@
 // following the naming style of airbnb guide - jsx (js would suffice but jsx preferred)
 
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  // react useState ...
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      // using spread operator
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
       {/* <Note /> */}
-      {notes.map(noteItem => (
+      {/* {notes.map((noteItem) => (
         <Note
           key={noteItem.key}
           title={noteItem.title}
           content={noteItem.content}
         />
-      ))} 
+      ))} */}
       {/* applied arrow function and maps */}
+
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
@@ -27,5 +60,3 @@ function App() {
 export default App;
 // NOTE:
 // The classes should be applied to html elements like div/h1/p... than react elements like <Header/>....
-
-
