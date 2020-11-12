2d593b063dfa166a4d4e21f6279387bd19b698a5
Surajv <vsuraj2405@gmail.com>
2020-11-12 15:35:01 +0530

    comment... icon removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import HighlightIcon from "@material-ui/icons/Highlight";
+
 function Header() {
   return (
     <header>
+      {/* <HighlightIcon /> */}
       <h1>Notes_Keeper App</h1>
     </header>
   );
